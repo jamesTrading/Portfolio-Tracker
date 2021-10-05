9ccc25788b6e80087ddfb68c073db0593fe09530
jamesTrading <65225964+jamesTrading@users.noreply.github.com>
2021-10-05 20:49:59 +1100

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import dash
+import base64
+import io
 import numpy as np
 from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_table
 import plotly.express as px
 import pandas as pd
 import pandas_datareader as pdr
@@ -17,8 +20,22 @@ import yfinance as yf
 import pandas_ta as pta
 
 
-def Model_Display(total_value, reason):
-    df = pd.read_csv('PortfolioModelJames.csv')
+def parse_contents(contents, filename):
+    content_type, content_string = contents.split(',')
+    decoded = base64.b64decode(content_string)
+    if 'csv' in filename:
+        # Assume that the user uploaded a CSV file
+        return pd.read_csv(
+            io.StringIO(decoded.decode('utf-8')))
+    elif 'xls' in filename:
+        # Assume that the user uploaded an excel file
+        return pd.read_excel(io.BytesIO(decoded))
+
+    
+
+def Model_Display(total_value, reason, rows):
+    df = pd.DataFrame(rows)
+    print(df)
     if reason == 'weights':
         x = 0
         outputlist = []
@@ -33,18 +50,18 @@ def Model_Display(total_value, reason):
     for h in df['Holdings']:
         data = yf.download(h,start =(date.today() - datetime.timedelta(days=2*365)), end = date.today())
         df2 = pd.DataFrame(data)
-        df1[h] = df2['Adj Close']
-        df1[h][0] = df2['Adj Close'][1]
+        df1[h] = df2['Close']
+        df1[h][0] = df2['Close'][1]
         amount_of_shares.append(df['Dollar Allocation'][count]/df1[h][0])
         count = count + 1
     market1 = yf.download("VOO",start =(date.today() - datetime.timedelta(days=2*365)), end = date.today())
-    united1 = total_value/market1['Adj Close'][0]
+    united1 = total_value/market1['Close'][0]
     market2 = yf.download("QQQ",start =(date.today() - datetime.timedelta(days=2*365)), end = date.today())
-    united2 = total_value/market2['Adj Close'][0]
+    united2 = total_value/market2['Close'][0]
     market3 = yf.download("IOZ.AX",start =(date.today() - datetime.timedelta(days=2*365)), end = date.today())
-    united3 = total_value/market3['Adj Close'][0]
+    united3 = total_value/market3['Close'][0]
     market4 = yf.download("VTS.AX",start =(date.today() - datetime.timedelta(days=2*365)), end = date.today())
-    united4 = total_value/market4['Adj Close'][0]
+    united4 = total_value/market4['Close'][0]
     market_portfolio1 = []
     market_portfolio2 = []
     market_portfolio3 = []
@@ -55,10 +72,10 @@ def Model_Display(total_value, reason):
     portfolio_value = []
     quick_sum = 0
     while count1 < len(df1[df['Holdings'][0]]):
-        market_portfolio1.append(united1*market1['Adj Close'][count1])
-        market_portfolio2.append(united2*market2['Adj Close'][count1])
-        market_portfolio3.append(united3*market3['Adj Close'][count1])
-        market_portfolio4.append(united4*market4['Adj Close'][count1])
+        market_portfolio1.append(united1*market1['Close'][count1])
+        market_portfolio2.append(united2*market2['Close'][count1])
+        market_portfolio3.append(united3*market3['Close'][count1])
+        market_portfolio4.append(united4*market4['Close'][count1])
         count2 = 0
         quick_sum = 0
         while count2 < len(df['Holdings']):
@@ -94,7 +111,7 @@ def Model_Display(total_value, reason):
         return fig
     return
 
-
+df = pd.read_csv('PortfolioModelJames.csv')
 
 #this creates the app -- imports the stylesheet
 app = dash.Dash(__name__,meta_tags=[{'property':'og:image','content':'https://i.ibb.co/P5RkK55/James-Charge-1.png'}])
@@ -119,33 +136,76 @@ app.layout = html.Div([
         dcc.Graph(id='my-graph')
         ],style={'width': '60%','display': 'inline-block'}),
     html.Div([
+        dcc.Upload(id='datatable-upload',
+        children=html.Div(['Drag and Drop or ',html.A('Select Files')]),style={'width': '100%', 'height': '60px', 'lineHeight': '60px','borderWidth': '1px', 'borderStyle': 'dashed',
+            'borderRadius': '5px', 'textAlign': 'center', 'margin': '10px'},),
         html.H4('Holdings and their Weights'),
-        html.Table(id = 'my-weights'),
+        dash_table.DataTable(id='datatable-upload-container'),
         html.H4('Returns of the Portfolio'),
-        html.Table(id = 'my-returns')
-        ],style={'width': '20%', 'float': 'right','display': 'inline-block','padding-right':'2%','padding-bottom':'2%'})
+        html.Table(id = 'my-returns')],style={'width': '20%', 'float': 'right','display': 'inline-block','padding-right':'2%','padding-bottom':'2%'}),
+    html.Div([
+        html.H4('Holdings and Weights Visualised'),
+        dcc.Graph(id='datatable-upload-graph')
+        ],style={'width': '30%', 'float': 'left','display': 'inline-block','padding-right':'2%','padding-bottom':'2%'}),
+    html.Div([
+        html.H4('Market Measures'),
+        html.Table(id = 'my-market')
+        ],style={'width': '30%', 'float': 'middle','display': 'inline-block','padding-right':'2%','padding-bottom':'2%'}),
 ])
 
 
 #This app callback updates the graph as per the relevant company
-@app.callback(Output('my-graph','figure'),[Input('totalvalue','value')])
-def update_graph(totalvalue):
-    fig = Model_Display(totalvalue, 'figure')
+@app.callback(Output('my-graph','figure'),[Input('totalvalue','value'),Input('datatable-upload-container', 'data')])
+def update_graph(totalvalue, rows):
+    fig = Model_Display(totalvalue, 'figure', rows)
     return fig
 
+
 #This app callback updates the graph as per the relevant company
-@app.callback(Output('my-weights','children'),[Input('totalvalue','value')])
-def update_weights(totalvalue):
-    outputlist = Model_Display(totalvalue, 'weights')
+@app.callback(Output('my-returns','children'),[Input('totalvalue','value'),Input('datatable-upload-container', 'data')])
+def update_returns(totalvalue, rows):
+    outputlist = Model_Display(totalvalue, 'returns', rows)
     # Header
     return [html.Tr(html.Td(output)) for output in outputlist]
 
 #This app callback updates the graph as per the relevant company
-@app.callback(Output('my-returns','children'),[Input('totalvalue','value')])
-def update_returns(totalvalue):
-    outputlist = Model_Display(totalvalue, 'returns')
+@app.callback(Output('my-market','children'),[Input('totalvalue','value'),Input('datatable-upload-container', 'data')])
+def update_market(totalvalue, rows):
+    outputlist = Model_Display(totalvalue, 'returns', rows)
     # Header
     return [html.Tr(html.Td(output)) for output in outputlist]
+
+@app.callback(Output('datatable-upload-container', 'data'),
+              Output('datatable-upload-container', 'columns'),
+              Input('datatable-upload', 'contents'),
+              State('datatable-upload', 'filename'))
+def update_output(contents, filename):
+    if contents is None:
+        return [{}], []
+    df = parse_contents(contents, filename)
+    return df.to_dict('records'), [{"name": i, "id": i} for i in df.columns]
+
+
+@app.callback(Output('datatable-upload-graph', 'figure'),
+              Input('datatable-upload-container', 'data'))
+def display_graph(rows):
+    df = pd.DataFrame(rows)
+
+    if (df.empty or len(df.columns) < 1):
+        return {
+            'data': [{
+                'x': [],
+                'y': [],
+                'type': 'bar'
+            }]
+        }
+    return {
+        'data': [{
+            'x': df[df.columns[0]],
+            'y': df[df.columns[1]],
+            'type': 'bar'
+        }]
+    }
 
 
 if __name__ == '__main__':
