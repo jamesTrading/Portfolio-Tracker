cd144ca5a34b9cb1cbf9ffe32348896b17247369
jamesTrading <65225964+jamesTrading@users.noreply.github.com>
2021-10-06 11:27:57 +1100

    Update app.py

--- app.py
+++ app.py
@@ -132,6 +132,8 @@ def Model_Display(total_value, reason, rows):
         df1['M2 Month'] = df1.rolling(window=21).sum()['M2 Ret']
         df1['M3 Month'] = df1.rolling(window=21).sum()['M3 Ret']
         df1['M4 Month'] = df1.rolling(window=21).sum()['M4 Ret']
+        print(df1['P Month'])
+        print(max(df1['P Month']))
         portfolio.append(round(max(df1['P Month']),2))
         m1.append(round(max(df1['M1 Month']),2))
         m2.append(round(max(df1['M2 Month']),2))
@@ -142,7 +144,8 @@ def Model_Display(total_value, reason, rows):
         m2.append(round(min(df1['M2 Month']),2))
         m3.append(round(min(df1['M3 Month']),2))
         m4.append(round(min(df1['M4 Month']),2))
-        portfolio.append(round(df1.loc[df1['P Ret']<0].std(),2))
+        dfp = df1.loc[df1['P Ret']<0]
+        portfolio.append(round(dfp.std(),2))
         m1.append(round(df1.loc[df1['M1 Ret']<0].std(),2))
         m2.append(round(df1.loc[df1['M2 Ret']<0].std(),2))
         m3.append(round(df1.loc[df1['M3 Ret']<0].std(),2))
